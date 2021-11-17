e7332f9ec78542336a33b7b0f93d5f300739eac2
Diogenes <diogenes.henrique.br@gmail.com>
2021-11-17 15:54:29 -0300

    Criando uma interface para cadastrar novos produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'produtos#index'
+  get 'produtos/new', to: 'produtos#new'
+  post 'produtos', to: 'produtos#create'
 end
