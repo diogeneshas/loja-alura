d2e1d1940d963bdf6688f912353d79db47d1b38c
Diogenes <diogenes.henrique.br@gmail.com>
2021-11-17 17:26:19 -0300

    Criando a busca de produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'produtos/busca', to: 'produtos#busca', as: :busca_produto
+  resources :produtos, only: [:new, :create, :destroy]
   root to: 'produtos#index'
-  get 'produtos/new', to: 'produtos#new'
-  post 'produtos', to: 'produtos#create'
-  delete 'produtos/:id', to: 'produtos#destroy', as: :produto
 end
