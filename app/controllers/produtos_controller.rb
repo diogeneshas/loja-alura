831ea4179295021c01699163156c392c934e647b
diogeneshas <diogenes.henrique.br@gmail.com>
2021-11-18 13:01:57 -0300

    Atualizando os produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -9,6 +9,26 @@ class ProdutosController < ApplicationController
     @departamentos = Departamento.all
   end
 
+  def edit
+    id = params[:id]
+    @produto = Produto.find(id)
+    @departamentos = Departamento.all
+    render :new
+  end
+
+  def update
+    id = params[:id]
+    @produto = Produto.find(id)
+    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+    if @produto.update valores
+      flash[:notice] = 'Produto atualizado com sucesso!'
+      redirect_to root_url
+    else
+      @departamentos = Departamento.all
+      render :new
+    end
+  end
+
     def create
        valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
 			 @produto = Produto.new valores
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :departamentos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'produtos/busca', to: 'produtos#busca', as: :busca_produto
-  resources :produtos, only: [:new, :create, :destroy]
+  resources :produtos, only: [:new, :create, :destroy, :edit, :update]
   root to: 'produtos#index'
 end
